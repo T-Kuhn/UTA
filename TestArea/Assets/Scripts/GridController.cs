52798b5260a7464da8dd9020c32ed785f43a9f69
T-Kuhn <tkuhn.mpu@gmail.com>
2017-04-19 23:47:58 +0900

    small changes

---
+++ TestArea/Assets/Scripts/GridController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridController : MonoBehaviour {
+
+    public GameObject FishPrefab;
+    [SerializeField] private float spacing = 6.0f;
+    public float Spacing { get { return spacing; } }
+    [SerializeField] private int rows = 4;
+    [SerializeField] private int columns = 3;
+
+	void Start () {
+        for (int i = 0; i <= columns; i++) {
+            for (int k = 0; k <= rows; k++) {
+                GameObject GridFish = Instantiate(FishPrefab) as GameObject;
+                GridFish.transform.parent = transform;
+                // Get child's script
+                var script = GridFish.GetComponent<GridFish>();
+                script.setParams(i, k, this);
+            }
+        }
+
+
+
+    }
+	
+	void Update () {
+		
+	}
+}
---
+++ TestArea/Assets/Scripts/GridFish.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridFish : MonoBehaviour {
+
+    private Vector2 gridPos;
+    private GridController gridController;
+
+    public void setParams(int x, int y, GridController parentRef)
+    {
+        gridController = parentRef;
+        gridPos = new Vector2((float)x, (float)y);
+        transform.localPosition = gridController.Spacing * (new Vector3((float)x, (float)y, 0));
+    }
+	
+	void Update () {
+		
+	}
+}
